c37ab3d2197983dd4fb53e1dd3528692f2285012
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-08-18 20:39:56 +1000

    get newly created screenshots

--- src/screenshots.ts
+++ src/screenshots.ts
@@ -1,26 +1,34 @@
 import { glob } from "glob";
 import path from "node:path";
 import fs from "node:fs/promises";
+import { debuglog } from "node:util";
 
-export interface ModifiedScreenshot {
-  absolutePath: string;
-  mtimeMs: number;
-}
+const debug = debuglog("screenshots");
 
-export async function getModifiedScreenshots(
+export async function getNewlyCreatedScreenshots(
   projectPath: string,
   // unix timestamp
   { after }: { after: number }
-): Promise<ModifiedScreenshot[]> {
+): Promise<string[]> {
   const pngs = await glob(path.join(projectPath, "**/*.png"));
+  debug("found %o", pngs);
   const withMetadata = await Promise.all(
     pngs.map(async (png) => {
       return {
-        mtimeMs: (await fs.stat(png)).mtimeMs,
+        ctimeMs: (await fs.stat(png)).ctimeMs,
         absolutePath: png,
       };
     })
   );
 
-  return withMetadata.filter((x) => x.mtimeMs > after);
+  const createdAfterStart = withMetadata.reduce<string[]>((acc, curr) => {
+    if (curr.ctimeMs > after) {
+      return acc.concat(curr.absolutePath);
+    }
+    return acc;
+  }, []);
+
+  debug("screenshots created after run started %o", createdAfterStart);
+
+  return createdAfterStart;
 }
