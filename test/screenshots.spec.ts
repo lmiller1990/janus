864c9d09751b7dee1c0c7338986f4b036a246479
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-08-18 20:41:19 +1000

    feat: get newly created screenshots (#2)
    * types
    
    * update build
    
    * get newly created screenshots
    
    * naming

--- test/screenshots.spec.ts
+++ test/screenshots.spec.ts
@@ -1,31 +1,24 @@
-import {
-  ModifiedScreenshot,
-  getModifiedScreenshots,
-} from "../src/screenshots.js";
+import { getNewlyCreatedScreenshots } from "../src/screenshots.js";
 import { describe, it } from "node:test";
 import { setupFixtureProject } from "./utils.js";
 import fs from "node:fs/promises";
 import path from "node:path";
 import assert from "node:assert";
 
-describe("getModifiedScreenshots", () => {
+const delay = () => new Promise((res) => setTimeout(res, 100));
+
+describe("getNewlyCreatedScreenshots", () => {
   it("gets png images modified after a given time", async () => {
     const dir = await setupFixtureProject("screenshots");
     const img = path.join(dir, "img-1.png");
+    await delay();
 
     const start = new Date().getTime();
-    await fs.utimes(img, new Date().getTime(), new Date().getTime());
-    const newStat = await fs.stat(img);
+    const newImg = path.join(dir, "new-image.png");
+    await fs.copyFile(img, newImg);
 
-    const expected: ModifiedScreenshot[] = [
-      {
-        absolutePath: img,
-        mtimeMs: newStat.mtimeMs,
-      },
-    ];
+    const actual = await getNewlyCreatedScreenshots(dir, { after: start });
 
-    const actual = await getModifiedScreenshots(dir, { after: start });
-
-    assert.deepEqual(actual, expected);
+    assert.deepEqual(actual, [newImg]);
   });
 });
